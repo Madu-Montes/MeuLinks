87d7d26c59a33d6dd2ac861c83a8a5db87f8f2db
Madu-Montes <monteseduarda56@gmail.com>
2023-01-30 21:23:26 -0300

    projeto finalizado

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-
-var valorEmDolar = 30;
+var valorEmDolar = window.prompt('Digite o valor em dolar? ')
 var cotacaoDoDolar = 5.12;
 
 var valorEmReal = valorEmDolar * cotacaoDoDolar;
 valorEmReal = valorEmReal.toFixed(2);  //arredondando o valor para duas casas decimais
 
-alert('R$' + valorEmReal);
+alert('R$ ' + valorEmReal);

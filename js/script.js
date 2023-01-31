423b47e5ff26aa74b687425c4b54f3800484b997
Madu-Montes <monteseduarda56@gmail.com>
2023-01-30 21:20:08 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-var valorEmDolar = 30;
-var cotacaoDoDolar = 5.32;
-valorEmDolar*cotacaoDoDolar
 
-alert(valorEmDolar);
+var valorEmDolar = 30;
+var cotacaoDoDolar = 5.12;
+
+var valorEmReal = valorEmDolar * cotacaoDoDolar;
+valorEmReal = valorEmReal.toFixed(2);  //arredondando o valor para duas casas decimais
+
+alert('R$' + valorEmReal);
